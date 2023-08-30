28ee9e0022060789557244007b2a3c34cc0d8e43
lindeneg <christian@lindeneg.org>
2023-08-30 21:52:19 +0200

    migrations

--- prisma/migrations/20230830193149_initial/migration.sql
+++ prisma/migrations/20230830193149_initial/migration.sql
@@ -6,19 +6,19 @@ CREATE TYPE "NavItemAlignment" AS ENUM ('RIGHT', 'LEFT');
 
 -- CreateTable
 CREATE TABLE "User" (
-    "_id" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "photo" TEXT,
     "role" "Role" NOT NULL DEFAULT 'USER',
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("_id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "NavigationItem" (
-    "_id" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
     "navigationId" TEXT,
     "name" TEXT NOT NULL,
     "href" TEXT NOT NULL,
@@ -26,51 +26,69 @@ CREATE TABLE "NavigationItem" (
     "alignment" "NavItemAlignment" NOT NULL DEFAULT 'RIGHT',
     "newTab" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "NavigationItem_pkey" PRIMARY KEY ("_id")
+    CONSTRAINT "NavigationItem_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Navigation" (
-    "_id" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
     "brandName" TEXT NOT NULL,
 
-    CONSTRAINT "Navigation_pkey" PRIMARY KEY ("_id")
+    CONSTRAINT "Navigation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PageSection" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "position" INTEGER NOT NULL DEFAULT 0,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "pageId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "PageSection_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Page" (
-    "_id" TEXT NOT NULL,
-    "slug" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "sections" TEXT[],
     "published" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Page_pkey" PRIMARY KEY ("_id")
+    CONSTRAINT "Page_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Post" (
-    "_id" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
     "published" BOOLEAN NOT NULL DEFAULT false,
-    "tags" TEXT[],
     "thumbnail" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Post_pkey" PRIMARY KEY ("_id")
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
--- AddForeignKey
-ALTER TABLE "NavigationItem" ADD CONSTRAINT "NavigationItem_navigationId_fkey" FOREIGN KEY ("navigationId") REFERENCES "Navigation"("_id") ON DELETE SET NULL ON UPDATE CASCADE;
+-- CreateIndex
+CREATE UNIQUE INDEX "Page_name_key" ON "Page"("name");
 
 -- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("_id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "NavigationItem" ADD CONSTRAINT "NavigationItem_navigationId_fkey" FOREIGN KEY ("navigationId") REFERENCES "Navigation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PageSection" ADD CONSTRAINT "PageSection_pageId_fkey" FOREIGN KEY ("pageId") REFERENCES "Page"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
